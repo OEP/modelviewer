9600e2fda9be4889d02b155205ee80de0950e238
Paul Kilgo <paulkilgo@gmail.com>
2015-09-15 19:36:32 -0400

    Start using GLUT

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 CXXFLAGS = -std=c++11 -Wall -Weffc++
+LDLIBS = -lglut -framework OpenGL
 LD = clang++
 CC = clang++
 CXX = clang++
--- mv.cpp
+++ mv.cpp
@@ -1,11 +1,47 @@
 #include <iostream>
 #include <fstream>
 
+#include <GLUT/glut.h>
+
 #include "model.h"
+
+Model *g_model;
+
+void display()
+{
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glBegin(GL_TRIANGLES);
+    Vertex x, y, z;
+    for(auto it = g_model->begin_faces(); it < g_model->end_faces(); it++) {
+        auto &face = *it;
+        for(size_t i = 0; i < 3; i++) {
+            face.getVertex(i, x, y, z);
+            glVertex3f(x, y, z);
+        }
+    }
+    glEnd();
+}
+
+void reshape(int width, int height)
+{
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+}
 
 int main(int argc, char **argv)
 {
-    std::ifstream is("/home/pkilgo/models/unitsphere.obj");
-    Model::read(is);
+    glutInit(&argc, argv);
+    glutCreateWindow("modelviewer");
+
+    std::ifstream is("models/unitsphere.obj");
+    g_model = Model::read(is);
+
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+
+    glutMainLoop();
+
     return 0;
 }
