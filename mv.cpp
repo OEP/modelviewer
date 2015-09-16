864a8222af77a2e93d376315979f39a094e5fda9
Paul Kilgo <paulkilgo@gmail.com>
2015-09-15 21:45:29 -0400

    work on mouse motions

--- mv.cpp
+++ mv.cpp
@@ -15,7 +15,75 @@
 #define MV_HEIGHT 480
 #define MV_WIDTH (MV_HEIGHT * MV_ASPECT)
 
+struct vector {
+    GLfloat x, y, z;
+};
+
 Model *g_model;
+
+struct vector g_translate = {0, 0, -2};
+struct vector g_rotate = {0, 0, 0};
+struct vector g_scale = {1, 1, 1};
+
+struct vector g_last_mouse_pos = {0, 0, 0};
+
+enum CameraState { NONE, TUMBLE, PAN, ZOOM };
+CameraState g_camera_state = NONE;
+
+void mouse(int button, int state, int x, int y)
+{
+    if(state == GLUT_DOWN && g_camera_state == NONE) {
+        switch(button) {
+        case GLUT_LEFT_BUTTON:
+            g_camera_state = TUMBLE;
+            break;
+        default:
+            break;
+        }
+        return;
+    }
+
+    /*
+     * The remainder of code will unset the camera state only if the button is
+     * being released.
+     */
+    if(state != GLUT_UP) {
+        return;
+    }
+
+    if(g_camera_state == TUMBLE && button == GLUT_LEFT_BUTTON) {
+        g_camera_state = NONE;
+    }
+}
+
+void update_mouse_position(int x, int y)
+{
+    g_last_mouse_pos.x = x;
+    g_last_mouse_pos.y = y;
+}
+
+void motion(int x, int y)
+{
+    int dx = x - g_last_mouse_pos.x;
+    int dy = y - g_last_mouse_pos.y;
+    switch(g_camera_state) {
+    case TUMBLE:
+        g_rotate.y += dx;
+        g_rotate.x += dy;
+        glutPostRedisplay();
+        break;
+    case PAN:
+    case ZOOM:
+    case NONE:
+        break;
+    }
+    update_mouse_position(x, y);
+}
+
+void passive_motion(int x, int y)
+{
+    update_mouse_position(x, y);
+}
 
 void display()
 {
@@ -32,7 +100,11 @@ void display()
     glColor3f(1, 1, 1);
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-    glTranslatef(0, 0, -2);
+    glTranslatef(g_translate.x, g_translate.y, g_translate.z);
+    glRotatef(g_rotate.x, 1, 0, 0);
+    glRotatef(g_rotate.y, 0, 1, 0);
+    glRotatef(g_rotate.z, 0, 0, 1);
+    glScalef(g_scale.x, g_scale.y, g_scale.z);
 
     glBegin(GL_POINTS);
     Vertex x, y, z;
@@ -87,6 +159,9 @@ int main(int argc, char **argv)
     glutReshapeWindow(MV_WIDTH, MV_HEIGHT);
 
     glutDisplayFunc(display);
+    glutMouseFunc(mouse);
+    glutMotionFunc(motion);
+    glutPassiveMotionFunc(passive_motion);
 
     glutMainLoop();
 
